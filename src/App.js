b96b91ea67b5cf24a5579981ef5b87f28fed6dac
Sergey Georgiev <sgrgv_art@mail.ru>
2025-01-13 12:46:55 +0300

    Установка и настройка tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <h1 className="text-3xl font-bold underline bg-yellow-300">Hello world!</h1>
+  );
 }
 
 export default App;
